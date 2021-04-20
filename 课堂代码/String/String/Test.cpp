9cc2789e7a816c709a5649cb61b030f32255ff38
yjh <2445542626@qq.com>
2021-04-20 22:50:45 +0800

    jiaru

--- 课堂代码/String/String/Test.cpp
+++ 课堂代码/String/String/Test.cpp
@@ -13,9 +13,50 @@ void stringtest(){
 	str.resize(10, 'a');
 	cout << str.size() << endl;
 	cout << str.capacity() << endl;//将s中有效字符个数增加到10个，多出位置用'a'进行填充
+	str.resize(20);
+	cout << str.size() << endl;
+	cout << str.capacity() << endl;
+	str.resize(5);
+	cout << str.size() << endl;
+	cout << str.capacity() << endl;
+	cout << str<<endl;
+	str.reserve(100);
+	cout << str.size() << endl;
+	cout << str.capacity() << endl;
 }	
+// 利用reserve提高插入数据的效率，避免增容带来的开销
+void testpushback(){
+	string s;
+    size_t sz = s.capacity();
+	cout << sz << endl;
+	for (int i = 0; i < 100; i++){
+		s.push_back('c');
+		if (sz != s.capacity()){
+			sz = s.capacity();
+			cout << "change capacity" << sz << endl;
+		}
+		}
+}
+void testpushbackreserve(){
+	string s;
+	s.reserve(100);
+	size_t sz = s.capacity();
+	cout << sz << endl;
+	for (int i = 0; i < 100; i++){
+		s.push_back('c');
+		if (sz != s.capacity()){
+			sz = s.capacity();
+			cout << "change capacity" << sz << endl;
+		}
+	}
+}
 int main(){
 	stringtest();
+	testpushback();
+	testpushbackreserve();
 	system("pause");
 	return 0;
 }
+
+
+
